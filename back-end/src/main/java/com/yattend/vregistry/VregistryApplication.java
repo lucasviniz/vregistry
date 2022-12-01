a0bac6316820ee37a47e931263e6f62b085fe5c8
Lucas Vinicius <howaitosame13@gmail.com>
2022-12-01 20:06:36 -0300

    commit initial

---
+++ back-end/src/main/java/com/yattend/vregistry/VregistryApplication.java
@@ -0,0 +1,13 @@
+package com.yattend.vregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VregistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VregistryApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/yattend/vregistry/VregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yattend.vregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VregistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
