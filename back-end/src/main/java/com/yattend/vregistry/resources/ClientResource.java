553157685cbaa55ca596c7efa5949b5e665747d2
Lucas Vinicius <howaitosame13@gmail.com>
2022-12-01 21:56:08 -0300

    resourceException, find clients by id

--- back-end/src/main/java/com/yattend/vregistry/resources/ClientResource.java
+++ back-end/src/main/java/com/yattend/vregistry/resources/ClientResource.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ public class ClientResource {
 	@Autowired
 	private ClientService service;
 	
+	
 	@GetMapping
 	public ResponseEntity<Page<ClientDTO>> findAll(
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
@@ -32,5 +34,14 @@ public class ClientResource {
 		
 		Page<ClientDTO> list = service.findAllPaged(pageRequest);
 		return ResponseEntity.ok().body(list);
+		
+	
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
+		ClientDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
 }
--- back-end/src/main/java/com/yattend/vregistry/services/ClientService.java
+++ back-end/src/main/java/com/yattend/vregistry/services/ClientService.java
@@ -1,5 +1,7 @@
 package com.yattend.vregistry.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.yattend.vregistry.dto.ClientDTO;
 import com.yattend.vregistry.entities.Client;
 import com.yattend.vregistry.repositories.ClientRepository;
+import com.yattend.vregistry.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -21,6 +24,13 @@ public class ClientService {
 		Page<Client> list = repository.findAll(pageRequest);
 		
 		return list.map(x -> new ClientDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
 	}
 	
 }
